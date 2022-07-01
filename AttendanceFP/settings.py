4334c89917e154b67ff5b3c35eee4bbc5ae5c350
otace1 <otace1@gmail.com>
2022-07-02 00:04:47 +0200

    Requirement

--- AttendanceFP/settings.py
+++ AttendanceFP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
