0c0ebc73e9a8cf9b600ecd3b8cc818be48452b24
otace1 <49515497+otace1@users.noreply.github.com>
2022-07-03 01:01:20 +0200

    Update settings.py

--- AttendanceFP/settings.py
+++ AttendanceFP/settings.py
@@ -154,12 +154,12 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = (BASE_DIR / 'static',)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 # Default primary key field type
