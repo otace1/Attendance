e99ccee700730337d34eaf9ad36d1a4fd8ae89c6
otace1 <49515497+otace1@users.noreply.github.com>
2022-07-03 07:41:31 +0200

    Update settings.py
    Dbs Changes

--- AttendanceFP/settings.py
+++ AttendanceFP/settings.py
@@ -105,7 +105,11 @@ DATABASES = {
         'HOST': 'private-db-mysql-sfo2-99144-do-user-4618658-0.b.db.ondigitalocean.com',
         'PORT': '25060',
         'OPTIONS': {
-            'ssl': {'ca': '/app/ca-certificate.crt'},
+            'ssl': {
+                'ca': 'ca-certificate.crt',
+                'cert': None,
+                'key': None,
+            },
             "charset": "utf8mb4",
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
